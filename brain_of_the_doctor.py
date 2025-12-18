ac10e66daa8ba29c13fd302aeb3e376ff8174d1c
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-12-18 16:01:40 +0530

    Add image to base64 conversion function

--- brain_of_the_doctor.py
+++ brain_of_the_doctor.py
@@ -1,5 +1,11 @@
 # Step 1: Setup GROQ API Key
 import os
 GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+
 # Step 2: Convert image to required format
+import base64
+image_path='acne.jpg'
+image_file=open(image_path, "rb") #read binary
+encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
 # Step 3: Setup Multimodal LLM
