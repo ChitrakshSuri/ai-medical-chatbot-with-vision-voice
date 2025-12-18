7f7042d09162a9e9522b5240982156b141f8fda4
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-12-18 18:16:46 +0530

    phase-2: add speech-to-text using Groq Whisper

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -3,6 +3,7 @@ import logging
 import speech_recognition as sr
 from pydub import AudioSegment
 from io import BytesIO
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -40,4 +41,22 @@ def record_audio(file_path, timeout=20, phrase_time_limit=None):
 audio_filepath="patient_voice_test_for_patient.mp3"
 record_audio(file_path=audio_filepath)
 
-# Step 2: Steup speech to text -stt model for transcription
+#Step2: Setup Speech to text–STT–model for transcription
+import os
+from groq import Groq
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+stt_model="whisper-large-v3"
+
+# def transcribe_with_groq(stt_model, audio_filepath, GROQ_API_KEY):
+client=Groq(api_key=GROQ_API_KEY)
+
+audio_file=open(audio_filepath, "rb")
+transcription=client.audio.transcriptions.create(
+    model=stt_model,
+    file=audio_file,
+    language="en"
+)
+
+# return transcription.text
+print(transcription.text)
